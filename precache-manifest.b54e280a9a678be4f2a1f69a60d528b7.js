0ac823589bc86e08ae6bea8c8cbafdc332e8ce52
mostafaibrahem <mostafa.ibrahem@yahoo.com>
2020-05-11 15:37:27 +0200

    Updates

---
+++ precache-manifest.b54e280a9a678be4f2a1f69a60d528b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cef0cf5319871a8c4dd4de0539a16a",
+    "url": "/Simple-Store/index.html"
+  },
+  {
+    "revision": "a4b730fc179a0e7bfaaf",
+    "url": "/Simple-Store/static/css/2.3ff54426.chunk.css"
+  },
+  {
+    "revision": "d3da02667c18073d2eef",
+    "url": "/Simple-Store/static/css/main.96923f1c.chunk.css"
+  },
+  {
+    "revision": "a4b730fc179a0e7bfaaf",
+    "url": "/Simple-Store/static/js/2.f5b58b2f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Simple-Store/static/js/2.f5b58b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3da02667c18073d2eef",
+    "url": "/Simple-Store/static/js/main.43609e0f.chunk.js"
+  },
+  {
+    "revision": "d873e41cbf3cf5dab5f6",
+    "url": "/Simple-Store/static/js/runtime-main.d9846e1e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Simple-Store/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Simple-Store/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Simple-Store/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Simple-Store/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Simple-Store/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
